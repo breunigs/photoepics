1028a99cab5436db0c310b3f2f5cf7e7d83d0984
Stefan Breunig <stefan-github@yrden.de>
2018-12-31 16:15:38 +0100

    support gpx with multiple tracks

--- cmd_load.go
+++ cmd_load.go
@@ -13,47 +13,49 @@ import (
 func cmdLoad() *cobra.Command {
 	var inputFilePath string
 	var mapConf mapillary.Config
+	var trackId int
 
 	cmd := &cobra.Command{
 		Use:   "load",
 		Short: "Loads images along the given file. Also calculates desirability for the images it finds.",
 		Run: func(cmd *cobra.Command, args []string) {
-			runCmdLoad(mapConf, inputFilePath)
+			runCmdLoad(mapConf, inputFilePath, trackId)
 		},
 	}
 	cmd.Flags().StringVarP(&inputFilePath, "input", "i", "", "input file for which to generate a photo sequence")
 	cmd.MarkFlagRequired("input")
-	requireApiKey(mapConf, cmd)
-	filterByUserName(mapConf, cmd)
-	filterByDate(mapConf, cmd)
+	requireApiKey(&mapConf, cmd)
+	filterByUserName(&mapConf, cmd)
+	filterByDate(&mapConf, cmd)
+	cmd.Flags().IntVarP(&trackId, "track", "", -1, "If the input file has more than one track, use this to specify the index of the desired one. It will be ignored if there is only one track.")
 
 	return cmd
 }
 
-func runCmdLoad(mapConf mapillary.Config, inputFilePath string) {
+func runCmdLoad(mapConf mapillary.Config, inputFilePath string, trackId int) {
 	db := dgraph.NewClient()
 
 	if mapillary.PhotoCount(db) > 0 {
 		log.Fatalf("Tried to load data, but database is not empty. This will lead to wrong results since the entries depend on the given input file. Please purge the DB.")
 	}
-	downloadAlong(mapConf, db, inputFilePath)
+	downloadAlong(mapConf, db, inputFilePath, trackId)
 }
 
-func requireApiKey(mapConf mapillary.Config, cmd *cobra.Command) {
+func requireApiKey(mapConf *mapillary.Config, cmd *cobra.Command) {
 	cmd.Flags().StringVar(&mapConf.ApiKey, "api-key", "", "Mapillary API Key")
 	cmd.MarkFlagRequired("api-key")
 }
 
-func filterByUserName(mapConf mapillary.Config, cmd *cobra.Command) {
+func filterByUserName(mapConf *mapillary.Config, cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&mapConf.FilterUsers, "filter-users", "", "", "only use photos from these Mapillary users. Comma separated.")
 }
 
-func filterByDate(mapConf mapillary.Config, cmd *cobra.Command) {
+func filterByDate(mapConf *mapillary.Config, cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&mapConf.FilterNewer, "filter-newer", "", "", "only use sequences newer than this date. Format YYYY-MM-DD.")
 }
 
-func downloadAlong(mapConf mapillary.Config, db dgraph.Wrapper, inputFilePath string) {
-	lineStr, err := trackFromFile(inputFilePath)
+func downloadAlong(mapConf mapillary.Config, db dgraph.Wrapper, inputFilePath string, trackId int) {
+	lineStr, err := trackFromFile(inputFilePath, trackId)
 	if err != nil {
 		log.Fatalf("Cannot extract GPS track from file: %+v", err)
 	}
--- parser.go
+++ parser.go
@@ -1,40 +1,82 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/paulmach/orb"
 	geojson "github.com/paulmach/orb/geojson"
+	"github.com/tkrajina/gpxgo/gpx"
 )
 
-func trackFromFile(filePath string) (orb.LineString, error) {
+func trackFromFile(filePath string, trackId int) (orb.LineString, error) {
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO: support GPX
-	return parseGeoJSON(data)
+	switch getFileEnding(filePath) {
+	case "gpx":
+		return parseGPX(data, trackId)
+	case "geojson":
+		return parseGeoJSON(data, trackId)
+	default:
+		return nil, errors.New("Unknown file extension")
+	}
 }
 
-func parseGeoJSON(data []byte) (orb.LineString, error) {
+func getFileEnding(path string) string {
+	path = strings.ToLower(path)
+	parts := strings.Split(path, ".")
+	return parts[len(parts)-1]
+}
+
+func parseGPX(data []byte, trackId int) (orb.LineString, error) {
+	gpxFile, err := gpx.ParseBytes(data)
+	if err != nil {
+		return nil, err
+	}
+
+	tracks := make([]orb.LineString, 0, len(gpxFile.Tracks))
+	trackDesc := make([]string, 0, len(gpxFile.Tracks))
+	for _, track := range gpxFile.Tracks {
+		ls := orb.LineString{}
+		for _, segment := range track.Segments {
+			for _, point := range segment.Points {
+				ls = append(ls, orb.Point{point.Longitude, point.Latitude})
+			}
+		}
+		trackDesc = append(trackDesc, track.Name)
+		tracks = append(tracks, ls)
+	}
+
+	return chooseTrack(tracks, trackDesc, trackId)
+}
+
+func parseGeoJSON(data []byte, trackId int) (orb.LineString, error) {
 	// TODO: what if the toplevel is not a FeatureCollection?
 	fc, err := geojson.UnmarshalFeatureCollection(data)
 	if err != nil {
 		return nil, err
 	}
 
-	ls := orb.LineString{}
+	tracks := []orb.LineString{}
+	trackDesc := []string{}
 	for _, feat := range fc.Features {
 		g := feat.Geometry
 		switch g.GeoJSONType() {
 		case "LineString":
-			ls = append(ls, g.(orb.LineString)...)
+			trackDesc = append(trackDesc, "LineString")
+			tracks = append(tracks, g.(orb.LineString))
 
 		case "MultiLineString":
-			for _, l := range g.(orb.MultiLineString) {
-				ls = append(ls, l...)
+			for i, l := range g.(orb.MultiLineString) {
+				trackDesc = append(trackDesc, fmt.Sprintf("MultiLineString #%d", i))
+				tracks = append(tracks, l)
 			}
 
 		default:
@@ -42,5 +84,32 @@ func parseGeoJSON(data []byte) (orb.LineString, error) {
 		}
 	}
 
-	return ls, nil
+	return chooseTrack(tracks, trackDesc, trackId)
+}
+
+func chooseTrack(tracks []orb.LineString, trackDesc []string, trackId int) (orb.LineString, error) {
+	if len(tracks) == 1 {
+		return tracks[0], nil
+	}
+
+	if len(tracks) == 0 {
+		return nil, errors.New("The given file does not contain any tracks")
+	}
+
+	if trackId >= len(tracks) {
+		errMsg := fmt.Sprintf("The given file only contains %d tracks, cannot select track %d", len(tracks), trackId)
+		return nil, errors.New(errMsg)
+	}
+
+	if trackId >= 0 {
+		return tracks[trackId], nil
+	}
+
+	chooser := "\nThe file you specified contains multiple tracks. Please choose which should be used:\n"
+	for idx, track := range tracks {
+		desc := trackDesc[idx] + fmt.Sprintf(" (length: %d)", len(track))
+		chooser += fmt.Sprintf("  %2d: %s\n", idx, desc)
+	}
+	chooser += fmt.Sprintf("\ne.g. %s --track 0", strings.Join(os.Args, " "))
+	return nil, errors.New(chooser)
 }
